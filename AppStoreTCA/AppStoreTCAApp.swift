e303986290298b1bcf54e5cd27e04a9511c5d810
Rimesh Jotaniya <rimesh.j@icloud.com>
2024-11-04 11:57:49 +0100

    Initial Commit

---
+++ AppStoreTCA/AppStoreTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreTCAApp.swift
+//  AppStoreTCA
+//
+//  Created by Rimesh Jotaniya on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
