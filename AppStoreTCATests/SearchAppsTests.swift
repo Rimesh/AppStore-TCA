dafee6f09c59c398f9e21f95a2e7d622005bccc1
Rimesh Jotaniya <rimesh.j@icloud.com>
2024-11-09 11:56:47 +0100

    Add SearchFeatureTests

--- AppStoreTCATests/SearchAppsTests.swift
+++ AppStoreTCATests/SearchAppsTests.swift
@@ -12,25 +12,63 @@ import Testing
 @testable import AppStoreTCA
 
 @MainActor
-struct SearchAppTests {
+struct SearchFeatureTests {
     @Test
-    func searchAndClearQuesry() async {
+    func searchAnyApp() async {
         let store = TestStore(initialState: SearchFeature.State()) {
-            Search()
+            SearchFeature()
         } withDependencies: { dependencies in
             dependencies.appStoreClient.search = { @Sendable _ in .mock }
+        }
+
+        await store.send(.searchbarFocusChanged(true)) {
+            $0.isSearchbarActive = true
         }
 
         await store.send(.searchQueryChanged("R")) {
             $0.searchQuery = "R"
         }
-        await store.send(.searchQueryChangeDebounced)
+        await store.send(.searchQueryChangeDebounced) {
+            $0.contentState = .loading
+        }
         await store.receive(\.searchResponse) {
             $0.results = .mock
+            $0.contentState = .results(.mock)
         }
+    }
+
+    @Test func clearSearchBar() async throws {
+        let store = TestStore(
+            initialState: SearchFeature.State(
+                searchQuery: "music",
+                isSearchbarActive: true
+            )
+        ) {
+            SearchFeature()
+        }
+
         await store.send(.searchQueryChanged("")) {
             $0.searchQuery = ""
-            $0.results = []
+            $0.contentState = .noResults
+        }
+        await store.send(.searchbarFocusChanged(false)) {
+            $0.isSearchbarActive = false
+            $0.contentState = .categories
+        }
+    }
+
+    @Test func selectCategory() async throws {
+        let store = TestStore(initialState: SearchFeature.State()) {
+            SearchFeature()
+        } withDependencies: { dependencies in
+            dependencies.appStoreClient.search = { @Sendable _ in .mock }
+        }
+        await store.send(.categories(.categoryButtonTapped(.music)))
+        await store.receive(\.categories.delegate.selectCategory, .music) {
+            $0.isSearchbarActive = true
+        }
+        await store.receive(\.searchQueryChanged, "music") {
+            $0.searchQuery = "music"
         }
     }
 }
