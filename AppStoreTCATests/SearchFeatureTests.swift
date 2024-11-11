85c2f06b4b333f74c0a7201d481f5d338ee3091f
Rimesh Jotaniya <rimesh.j@icloud.com>
2024-11-11 11:38:34 +0100

    Fix Search tests

--- AppStoreTCATests/SearchFeatureTests.swift
+++ AppStoreTCATests/SearchFeatureTests.swift
@@ -83,9 +83,7 @@ struct SearchFeatureTests {
             dependencies.appStoreClient.search = { @Sendable _ in .mock }
         }
         await store.send(.categories(.categoryButtonTapped(.music)))
-        await store.receive(\.categories.delegate.selectCategory, .music) {
-            $0.isSearchbarActive = false
-        }
+        await store.receive(\.categories.delegate.selectCategory, .music)
         await store.receive(\.searchQueryChanged, "music") {
             $0.searchQuery = "music"
         }
